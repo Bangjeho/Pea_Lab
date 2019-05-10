4abe0f222d2728c329521c5a34ef98c6150c0442
Bangjeho <fix9111@naver.com>
2019-05-10 21:08:46 +0900

    division codding

---
+++ Arm_Jiwon/src/kr/ac/sunmoon/pea/Item.java
@@ -0,0 +1,15 @@
+package kr.ac.sunmoon.pea;
+
+import java.util.ArrayList;
+
+public class Item {
+	private ArrayList<String> Item_name = new ArrayList<>();
+
+	public ArrayList<String> getItem_name() {
+		return Item_name;
+	}
+
+	public void setItem_name(ArrayList<String> item_name) {
+		Item_name = item_name;
+	}
+}
