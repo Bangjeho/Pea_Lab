ac8188237fcc4b4cde0870e0a9293518d517f0f7
Bangjeho <fix9111@naver.com>
2019-05-18 22:30:32 +0900

    multiple intersection test

--- Arm_jeho/src/kr/ac/sunmoon/pea/Relation.java
+++ Arm_jeho/src/kr/ac/sunmoon/pea/Relation.java
@@ -1,5 +1,6 @@
 package kr.ac.sunmoon.pea;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,6 +8,8 @@ public class Relation
 {
 	public static void main(String[] args)
 	{
+		long start_time = System.currentTimeMillis();
+		
 		HashMap<String, ArrayList<Integer>> hmRel = new HashMap<String, ArrayList<Integer>>();
 		
 		ArrayList<Integer> a = new ArrayList<Integer>();
@@ -28,28 +31,61 @@ public class Relation
 		c.add(500);
 		hmRel.put("c", c);
 		
-		// test 1
+		ArrayList<Integer> d = new ArrayList<Integer>();
+		d.add(100);
+		d.add(200);
+		d.add(300);
+		d.add(400);
+		d.add(500);
+		hmRel.put("d", d);
+		
+		// test 1 item a and item b intersection
 		ArrayList<String> x = new ArrayList<String>();
 		x.add("a");
 		x.add("b");
 		ArrayList<Integer> output = funcX(x, hmRel);
-		System.out.println("transaction with item a and item b >> " + output);
+		long end_time = System.currentTimeMillis();
+		System.out.println("transaction with item a and item b >> " + output + ", Execution time : " + (end_time - start_time) + "(ms)");
+
 		
-		// test 2
+		// test 2 item a and item c intersection
 		x = new ArrayList<String>();
 		x.add("a");
 		x.add("c");
-		output = funcX(x, hmRel);
-		System.out.println("transaction with item a and item c >> " + output);
+		ArrayList<Integer> output2 = funcX(x, hmRel);
+		end_time = System.currentTimeMillis();
+		System.out.println("transaction with item a and item c >> " + output2 + ", Execution time : " + (end_time - start_time) + "(ms)");
 		
 		
-		// test 3
+		// test 3 item b and item c intersection
 		x = new ArrayList<String>();
 		x.add("b");
 		x.add("c");
-		output = funcX(x, hmRel);
-		System.out.println("transaction with item b and item c >> " + output);
+		ArrayList<Integer> output3 = funcX(x, hmRel);
+		end_time = System.currentTimeMillis();
+		System.out.println("transaction with item b and item c >> " + output3 + ", Execution time : " + (end_time - start_time) + "(ms)");
 		
+		//test 4 item a and item b and item d intersection
+		x = new ArrayList<String>();
+		x.add("d");
+		ArrayList<Integer> output4 = funcY(x, hmRel, output);
+		end_time = System.currentTimeMillis();
+		System.out.println("transaction with item a and item b and item d >> " + output4 + ", Execution time : " + (end_time - start_time) + "(ms)");
+		
+		//test 5 item a and item b and item c intersection
+		x = new ArrayList<String>();
+		x.add("c");
+		ArrayList<Integer> output5 = funcY(x, hmRel, output);
+		end_time = System.currentTimeMillis();
+		System.out.println("transaction with item a and item b and item c >> " + output5 + ", Execution time : " + (end_time - start_time) + "(ms)");
+		
+		
+		//test6 item a and item b and item c and item d intersection
+		x = new ArrayList<String>();
+		x.add("d");
+		ArrayList<Integer> output6 = funcY(x, hmRel, output5);
+		end_time = System.currentTimeMillis();
+		System.out.println("transaction with item a and item b and item c and item d >> " + output6 + ", Execution time : " + (end_time - start_time) + "(ms)");
 		
 //		ArrayList<Person> people = new ArrayList<Person>();
 //		Person p_1 = new Person();
@@ -63,7 +99,7 @@ public class Relation
 //		for(int i=0; i<people.size(); i++)
 //			System.out.println(people.get(i).getName());
 	}
-	
+	// intersection item ? and item ? 
 	private static ArrayList<Integer> funcX(ArrayList<String> x, HashMap<String, ArrayList<Integer>> hmRel)
 	{
 		ArrayList<Integer> output = new ArrayList<Integer>();
@@ -83,6 +119,22 @@ public class Relation
 		
 		return Intersection;
 	}
+	
+	// Double intersection function method exam) ((a and b)and c)
+	private static ArrayList<Integer> funcY(ArrayList<String> x, HashMap<String, ArrayList<Integer>> hmRel, ArrayList<Integer> output){
+		ArrayList<Integer> output_x = new ArrayList<Integer>();
+		ArrayList<Integer> Intersection = new ArrayList<Integer>();
+		if(x.size() > 0)
+		{
+			String item = x.get(0);
+			output_x = hmRel.get(item);
+			ArrayList<Integer> transs = output;
+			Intersection = intersection(output_x,transs);
+		}
+		return Intersection;
+		
+	}
+	//find to intersection with item ? and item ? method 
     public static <Integer> ArrayList<Integer> intersection(ArrayList<Integer> output,ArrayList<Integer> transs) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
